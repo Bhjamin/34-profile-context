cf9d69d09e7d459598d5abd213c5e6ced6842076
Bhjamin <benjamin@hancock1.com>
2023-03-07 10:04:21 -0700

    created server files

---
+++ src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import { useContext} from 'react'
+import ProfileContext from '../store/profileContext'
+
+const Profile = () => {
+  const profile = useContext(ProfileContext)
+  return (
+    <div>
+      <p>Username: {profile.username}</p>
+      <button onClick={() => {profile.deleteUsername()}}>Delete Username</button>
+    </div>
+  )
+}
+
+export default Profile
---
+++ src/store/profileContext.js
@@ -0,0 +1,48 @@
+import { createContext, useState } from "react";
+
+const ProfileContext = createContext({
+  username: "",
+  updateUsername: () => {},
+  deleteUsername: () => {},
+});
+
+const getLocalData = () => {
+  const storedUsername = localStorage.getItem("username");
+
+  return storedUsername;
+};
+
+export const ProfileContextProvider = (props) => {
+  const storedUsername = getLocalData();
+
+  let initialUsername;
+  if (storedUsername) {
+    initialUsername = storedUsername;
+  }
+
+  const [username, setUsername] = useState(initialUsername);
+
+  const updateUsername = (newUsername) => {
+    setUsername(newUsername);
+    localStorage.setItem("username", newUsername);
+  };
+
+  const deleteUsername = () => {
+    setUsername("");
+    localStorage.removeItem('username')
+  };
+
+  let contextValue = {
+    username,
+    updateUsername,
+    deleteUsername,
+  };
+
+  return (
+    <ProfileContext.Provider value={contextValue}>
+      {props.children}
+    </ProfileContext.Provider>
+  );
+};
+
+export default ProfileContext;
